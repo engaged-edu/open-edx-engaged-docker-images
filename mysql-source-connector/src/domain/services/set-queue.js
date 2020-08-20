d12e8d7350ae519b4487346289382ecc6d0daa00
gustavo.santos97 <gustavoms97@hotmail.com>
2020-08-20 16:27:47 -0300

    feat: add error throwing with specific messages so that the flow break when it needs to

--- mysql-source-connector/src/domain/services/set-queue.js
+++ mysql-source-connector/src/domain/services/set-queue.js
@@ -1,12 +1,20 @@
 const { getQueueName } = require('../helpers/get-queue-name');
+const { APP_ERROR_MESSAGE } = require('../../constants');
 
-exports.setQueueFactory = ({ lowDb } = {}) => {
+exports.setQueueFactory = ({ lowDb, AppError } = {}) => {
   return {
     setQueue: ({ queue, dlq = false } = {}) => {
-      if (!Array.isArray(queue)) {
-        throw new Error('Invalid queue type');
+      try {
+        if (!Array.isArray(queue)) {
+          throw new Error('Invalid queue type');
+        }
+        return lowDb.set(getQueueName({ dlq }), queue).write();
+      } catch (setQueueError) {
+        throw new AppError({
+          message: APP_ERROR_MESSAGE.QUEUE.SET_ON_LOCAL_DB,
+          error: setQueueError,
+        }).flush();
       }
-      return lowDb.set(getQueueName({ dlq }), queue).write();
     },
   };
 };
