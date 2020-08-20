ac043a94ceca07dedbd7a9c2572c0a5717818190
gustavo.santos97 <gustavoms97@hotmail.com>
2020-08-20 16:24:09 -0300

    fix: fix both the aws and event bridge sdks so that it doesnt update the configuration more than once, causing the lib to lose values

--- mysql-source-connector/src/infra/sdk/aws.js
+++ mysql-source-connector/src/infra/sdk/aws.js
@@ -7,6 +7,7 @@ exports.configAWSSDK = ({ ENV } = {}) => {
   AWS.config.update({
     accessKeyId: ENV.ENGAGED_AWS_ACCESS_KEY,
     secretAccessKey: ENV.ENGAGED_AWS_SECRET_KEY,
+    region: ENV.ENGAGED_AWS_EVENTBRIDGE_BUS_REGION,
   });
   return { aws: AWS };
 };
--- mysql-source-connector/src/infra/sdk/event-bridge.js
+++ mysql-source-connector/src/infra/sdk/event-bridge.js
@@ -1,8 +1,7 @@
 /**
  * @param {{ aws: import('aws-sdk') }} params
  */
-exports.configAWSEventBridge = ({ ENV, aws }) => {
+exports.configAWSEventBridge = ({ aws }) => {
   const eventBridge = new aws.EventBridge();
-  eventBridge.config.update({ region: ENV.ENGAGED_AWS_EVENTBRIDGE_BUS_REGION });
   return { eventBridge };
 };
