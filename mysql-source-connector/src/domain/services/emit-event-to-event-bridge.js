7239c116e76187b1de8358a138d33646decf9755
gustavo.santos97 <gustavoms97@hotmail.com>
2020-08-20 16:21:27 -0300

    feat: add try/catch flow, and throws a specific error so that it is easier to track

--- mysql-source-connector/src/domain/services/emit-event-to-event-bridge.js
+++ mysql-source-connector/src/domain/services/emit-event-to-event-bridge.js
@@ -1,30 +1,39 @@
+const { APP_ERROR_MESSAGE } = require('../../constants');
+
 /**
  * @param {{ ENV: import('../../infra/config').ENV, eventBridge: import('aws-sdk').EventBridge }}
  */
-exports.emitEventToEventBridgeFactory = ({ ENV, eventBridge }) => {
+exports.emitEventToEventBridgeFactory = ({ ENV, eventBridge, AppError }) => {
   return {
     emitEventToEventBridge: async ({ event, eventType }) => {
-      const eventToSend = {
-        EventBusName: ENV.ENGAGED_AWS_EVENTBRIDGE_BUS_NAME,
-        Source: ENV.ENGAGED_AWS_EVENTBRIDGE_PRODUCER_NAME,
-        Time: new Date(),
-        DetailType: eventType,
-        Detail: JSON.stringify({
-          ...event,
-          server_identifier: ENV.ENGAGED_SERVER_IDENTIFIER,
-        }),
-      };
-      const res = await eventBridge.putEvents({ Entries: [eventToSend] }).promise();
-      if (
-        !res ||
-        res.FailedEntryCount !== 0 ||
-        !Array.isArray(res.Entries) ||
-        res.Entries.length !== 1 ||
-        typeof res.Entries[0].EventId !== 'string'
-      ) {
-        throw new Error('failed to emit event to event bridge');
+      try {
+        const eventToSend = {
+          EventBusName: ENV.ENGAGED_AWS_EVENTBRIDGE_BUS_NAME,
+          Source: ENV.ENGAGED_AWS_EVENTBRIDGE_PRODUCER_NAME,
+          Time: new Date(),
+          DetailType: eventType,
+          Detail: JSON.stringify({
+            ...event,
+            server_identifier: ENV.ENGAGED_SERVER_IDENTIFIER,
+          }),
+        };
+        const res = await eventBridge.putEvents({ Entries: [eventToSend] }).promise();
+        if (
+          !res ||
+          res.FailedEntryCount !== 0 ||
+          !Array.isArray(res.Entries) ||
+          res.Entries.length !== 1 ||
+          typeof res.Entries[0].EventId !== 'string'
+        ) {
+          throw new Error('failed to emit event to event bridge');
+        }
+        return { eventId: res.Entries[0].EventId };
+      } catch (emitToEventBridgeError) {
+        throw new AppError({
+          message: APP_ERROR_MESSAGE.EVENT.EMIT_TO_EVENTBRIDGE,
+          error: emitToEventBridgeError,
+        }).flush();
       }
-      return { eventId: res.Entries[0].EventId };
     },
   };
 };
