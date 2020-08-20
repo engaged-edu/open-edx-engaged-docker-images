55a3a717c5d7518069bb28aac40e4216175d70ce
gustavo.santos97 <gustavoms97@hotmail.com>
2020-08-20 16:29:54 -0300

    fix: add error throwing with specific message and fix the validation with the after/befofe attributes from affectedRow

--- mysql-source-connector/src/domain/services/handle-queue-event.js
+++ mysql-source-connector/src/domain/services/handle-queue-event.js
@@ -1,48 +1,58 @@
-exports.handleQueueEventFactory = ({ fetchUsersFromOpenEdx, emitEventToEventBridge }) => {
+const { APP_ERROR_MESSAGE } = require('../../constants');
+
+exports.handleQueueEventFactory = ({ fetchUsersFromOpenEdx, emitEventToEventBridge, AppError }) => {
   return {
     handleQueueEvent: async ({ event, eventAffectedRowUserField } = {}) => {
-      if (!event || !Array.isArray(event.affectedRows) || event.affectedRows.length === 0) {
-        throw new Error('Invalid event');
-      }
+      try {
+        if (!event || !Array.isArray(event.affectedRows) || event.affectedRows.length === 0) {
+          throw new Error('Invalid event');
+        }
 
-      // Percorrer lista de affectedrows do evento, pegando o id do aluno em cada after e adicionando em um array
-      const usersId = event.affectedRows
-        .map((affectedRow) => {
-          let user_id = undefined;
-          if (affectedRow.after[eventAffectedRowUserField]) {
-            user_id = affectedRow.after[eventAffectedRowUserField];
-          } else if (affectedRow.before[eventAffectedRowUserField]) {
-            user_id = affectedRow.before[eventAffectedRowUserField];
+        // Percorrer lista de affectedrows do evento, pegando o id do aluno em cada after e adicionando em um array
+        const usersId = event.affectedRows
+          .map((affectedRow) => {
+            let user_id = undefined;
+            if (affectedRow.after && affectedRow.after[eventAffectedRowUserField]) {
+              user_id = affectedRow.after[eventAffectedRowUserField];
+            } else if (affectedRow.before && affectedRow.before[eventAffectedRowUserField]) {
+              user_id = affectedRow.before[eventAffectedRowUserField];
+            }
+            affectedRow.user = user_id;
+            return user_id;
+          })
+          .filter((value) => !!value);
+
+        const result = await fetchUsersFromOpenEdx({ usersId });
+        const users = result.reduce((users, user) => {
+          if (!user || typeof user.id !== 'number') {
+            throw new Error('Fetched user without id');
           }
-          affectedRow.user = user_id;
-          return user_id;
-        })
-        .filter((value) => !!value);
+          users[user.id] = user;
+          return users;
+        }, {});
 
-      const users = await fetchUsersFromOpenEdx({ usersId }).reduce((users, user) => {
-        if (!user || typeof user.id !== 'number') {
-          throw new Error('Fetched user without id');
-        }
-        users[user.id] = user;
-        return users;
-      }, {});
+        // Distribui em cada affectedrow seu aluno correspondente
+        event.affectedRows.forEach((affectedRow) => {
+          if (!affectedRow.user) {
+            return;
+          }
+          if (affectedRow.user in users) {
+            affectedRow.user = users[affectedRow.user];
+          }
+        });
 
-      // Distribui em cada affectedrow seu aluno correspondente
-      event.affectedRows.forEach((affectedRow) => {
-        if (!affectedRow.user) {
-          return;
-        }
-        if (affectedRow.user in users) {
-          affectedRow.user = users[affectedRow.user];
-        }
-      });
+        const { eventId } = await emitEventToEventBridge({
+          event,
+          eventType: event.table,
+        });
 
-      const { eventId } = await emitEventToEventBridge({
-        event,
-        eventType: event.table,
-      });
-
-      return { eventId };
+        return { eventId };
+      } catch (handleQueueEventError) {
+        throw new AppError({
+          message: APP_ERROR_MESSAGE.EVENT.HANDLE,
+          error: handleQueueEventError,
+        }).flush();
+      }
     },
   };
 };
