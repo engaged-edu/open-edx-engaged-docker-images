d108a570342bddb35c1a98feb9affeec6bf54ad8
gustavo.santos97 <gustavoms97@hotmail.com>
2020-08-20 16:22:28 -0300

    feat: add error handling and throwing, and fix the usage of joi.attempt

--- mysql-source-connector/src/domain/services/fetch-users-from-open-edx.js
+++ mysql-source-connector/src/domain/services/fetch-users-from-open-edx.js
@@ -1,9 +1,10 @@
 const joi = require('@hapi/joi');
+const { APP_ERROR_MESSAGE } = require('../../constants');
 
 /**
  * @param {{mysql: import('mysql').Connection}} params
  */
-exports.fetchUsersFromOpenEdxFactory = ({ mysql } = {}) => {
+exports.fetchUsersFromOpenEdxFactory = ({ mysql, AppError } = {}) => {
   return {
     fetchUsersFromOpenEdx: (params = {}) => {
       return new Promise((resolve, reject) => {
@@ -11,23 +12,41 @@ exports.fetchUsersFromOpenEdxFactory = ({ mysql } = {}) => {
           /**
            * @type {{ usersId: [] }}
            */
-          const { usersId } = joi.attempt(params, {
-            usersId: joi.array().items(joi.number().positive().integer().required()).required(),
-          });
+          const { usersId } = joi.attempt(
+            params,
+            joi.object({
+              usersId: joi.array().items(joi.number().positive().integer().required()).required(),
+            }),
+          );
           if (usersId.length === 0) {
             return resolve({ users: [] });
           }
           mysql.query(`select * from auth_user where id IN (${usersId.join(',')})`, (usersQueryError, users) => {
             if (usersQueryError) {
-              return reject(usersQueryError);
+              return reject(
+                new AppError({
+                  message: APP_ERROR_MESSAGE.OPEN_EDX_MYSQL.FETCH_USERS_QUERY,
+                  error: usersQueryError,
+                }).flush(),
+              );
             }
             if (!Array.isArray(users)) {
-              return reject(new Error('invalid mysql query response'));
+              return reject(
+                new AppError({
+                  message: APP_ERROR_MESSAGE.OPEN_EDX_MYSQL.FETCH_USERS_QUERY,
+                  error: new Error('invalid mysql query response'),
+                }).flush(),
+              );
             }
             return resolve(users.map((user) => ({ ...user })));
           });
-        } catch (error) {
-          return reject(error);
+        } catch (mysqlError) {
+          return reject(
+            new AppError({
+              message: APP_ERROR_MESSAGE.OPEN_EDX_MYSQL.FETCH_USERS_QUERY,
+              error: mysqlError,
+            }).flush(),
+          );
         }
       });
     },
