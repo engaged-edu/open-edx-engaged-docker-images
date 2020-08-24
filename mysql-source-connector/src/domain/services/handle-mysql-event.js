bef308a366bbf6be91242d19b582e83829965822
gustavo.santos97 <gustavoms97@hotmail.com>
2020-08-24 10:47:05 -0300

    fix: add error handling to the mysql event handler

--- mysql-source-connector/src/domain/services/handle-mysql-event.js
+++ mysql-source-connector/src/domain/services/handle-mysql-event.js
@@ -1,23 +1,30 @@
 const joi = require('@hapi/joi');
+const { APP_ERROR_MESSAGE } = require('../../constants');
 
-exports.handleMySQLEventFactory = ({ addEventToQueue, updateQueueConfiguration } = {}) => {
+exports.handleMySQLEventFactory = ({ addEventToQueue, updateQueueConfiguration, AppError } = {}) => {
   return {
     handleMySQLEvent: ({ event } = {}) => {
-      // TODO - envelopar com tratamento de erro
-      joi.assert(
-        event,
-        joi
-          .object({
-            binlogName: joi.string().required(),
-            nextPosition: joi.number().positive().integer().required(),
-          })
-          .unknown()
-          .required(),
-      );
-      addEventToQueue({ event });
-      const { nextPosition, binlogName } = event;
-      updateQueueConfiguration({ nextPosition, binlogName });
-      return;
+      try {
+        joi.assert(
+          event,
+          joi
+            .object({
+              binlogName: joi.string().required(),
+              nextPosition: joi.number().positive().integer().required(),
+            })
+            .unknown()
+            .required(),
+        );
+        addEventToQueue({ event });
+        const { nextPosition, binlogName } = event;
+        updateQueueConfiguration({ nextPosition, binlogName });
+        return;
+      } catch (handleMySQLEventError) {
+        throw new AppError({
+          message: APP_ERROR_MESSAGE.EVENT.HANDLE_MYSQL,
+          error: handleMySQLEventError,
+        });
+      }
     },
   };
 };
