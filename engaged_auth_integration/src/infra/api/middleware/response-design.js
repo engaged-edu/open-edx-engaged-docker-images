595c37d281d5b9dd02b2dd43ce426d01d0b0889f
gustavo.santos97 <gustavoms97@hotmail.com>
2020-08-28 19:15:30 -0300

    fix: fix the logic to know what error to show to the end user

--- engaged_auth_integration/src/infra/api/middleware/response-design.js
+++ engaged_auth_integration/src/infra/api/middleware/response-design.js
@@ -1,48 +1,29 @@
-const { APP_ERROR_KIND } = require('../../../constants');
+const { APP_ERROR_KIND_STATEGY } = require('../../../constants');
 
 exports.responseDesignAPIMiddlewareFactory = ({ AppError }) => {
   return {
     responseDesignAPIMiddleware: (req, res, next) => {
-
       res.success = (result) => {
         return res.status(200).send({
           status: 'OK',
-          result
+          result,
         });
-      }
+      };
       res.catch = function responseCatchError(error) {
-        if(error instanceof AppError) {
-          switch(error.kind) {
-            case APP_ERROR_KIND.NOT_FOUND:
-              return res.status(404).send({
-                status: 'NOT-FOUND',
-                error_message: error.message
-              });
-            case APP_ERROR_KIND.VALIDATION:
-              return res.status(400).send({
-                status: 'BAD-REQUEST',
-                error_message: error.message
-              });
-            case APP_ERROR_KIND.FORBIDDEN:
-              return res.status(400).send({
-                status: 'FORBIDDEN',
-                error_message: error.message
-              });
-            case APP_ERROR_KIND.UNEXPECTED:
-            default:
-              return res.status(500).send({
-                status: 'INTERNAL-ERROR',
-                error_message: error.message
-              });
-          }
+        if (error instanceof AppError) {
+          const { code, status } = APP_ERROR_KIND_STATEGY[error.kind];
+          return res.status(code).send({
+            status,
+            error_message: error.message,
+          });
         }
-        if(error instanceof Error) {
+        if (error instanceof Error) {
           return responseCatchError(new AppError(error));
         }
         return responseCatchError(new AppError(new Error('')));
-      }
+      };
 
       return next();
-    }
-  }
-}
+    },
+  };
+};
