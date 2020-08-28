26415c16fc2d6630a33fb2eea7bbb0d44a5b3c38
gustavo.santos97 <gustavoms97@hotmail.com>
2020-08-28 19:16:41 -0300

    feat: add the service to insert the auth token in the openedx auth table

---
+++ engaged_auth_integration/src/domain/services/create-open-edx-user-access-token.js
@@ -0,0 +1,82 @@
+const joi = require('@hapi/joi');
+const randomstring = require('randomstring');
+const moment = require('moment');
+const { APP_ERROR_CODE } = require('../../constants');
+
+const statement = `
+INSERT INTO openedx.oauth2_provider_accesstoken 
+(
+  token,
+  expires,
+  scope,
+  application_id,
+  user_id,
+  created,
+  updated,
+  source_refresh_token_id
+) VALUES(?,?,?,?,?,?,?,?)
+`.trim();
+
+/**
+ * @param {{mysql: import('mysql').Connection}} params
+ */
+exports.createOpenEdxUserAccessTokenFactory = ({ mysql, AppError } = {}) => {
+  return {
+    createOpenEdxUserAccessToken: (params = {}) => {
+      return new Promise((resolve, reject) => {
+        try {
+          /**
+           * @type {{ user_id: '' }}
+           */
+          const { user_id } = joi.attempt(
+            params,
+            joi
+              .object({
+                user_id: joi.number().required(),
+              })
+              .required(),
+          );
+          mysql.query(
+            statement,
+            [
+              randomstring.generate(30),
+              moment(moment.now()).add(1, 'day').toDate(),
+              'user_id email profile',
+              1,
+              user_id,
+              new Date(),
+              new Date(),
+              null,
+            ],
+            (usersQueryError, result) => {
+              if (usersQueryError) {
+                return reject(
+                  new AppError({
+                    code: APP_ERROR_CODE.INSERT_OPEN_EDX_AUTH_TOKEN,
+                    error: usersQueryError,
+                  }),
+                );
+              }
+              if (!result) {
+                return reject(
+                  new AppError({
+                    code: APP_ERROR_CODE.INSERT_OPEN_EDX_AUTH_TOKEN,
+                    error: new Error('invalid mysql query response'),
+                  }),
+                );
+              }
+              return resolve({ ...result });
+            },
+          );
+        } catch (mysqlError) {
+          return reject(
+            new AppError({
+              code: APP_ERROR_CODE.INSERT_OPEN_EDX_AUTH_TOKEN,
+              error: mysqlError,
+            }),
+          );
+        }
+      });
+    },
+  };
+};
