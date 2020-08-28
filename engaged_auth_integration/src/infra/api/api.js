f9bdfed0edc3213d111666836dc9d33f12a928c7
gustavo.santos97 <gustavoms97@hotmail.com>
2020-08-28 19:13:09 -0300

    feat: add the stopServer handler, add information to the rejection error and got the result from listen so that we can close the express connection later

--- engaged_auth_integration/src/infra/api/api.js
+++ engaged_auth_integration/src/infra/api/api.js
@@ -1,18 +1,32 @@
 const Express = require('express');
+const { APP_ERROR_CODE, APP_ERROR_KIND } = require('../../constants');
 
-exports.startAPIServer = ({ ENV, apiRouter } = {}) => {
+exports.startAPIServer = ({ ENV, apiRouter, AppError } = {}) => {
   return new Promise((resolve, reject) => {
     const api = Express();
     api.use(Express.json({ limit: '1mb' }));
     apiRouter({ api });
-    api.listen(ENV.API_SERVER_PORT, (serverError) => {
-      if(serverError) {
-        return reject(serverError);
+    const server = api.listen(ENV.API_SERVER_PORT, (serverError) => {
+      if (serverError) {
+        return reject(
+          new AppError({ error: serverError, kind: APP_ERROR_KIND.DEFAULT, CODE: APP_ERROR_CODE.API_START }),
+        );
       }
-      return resolve({ api });
+      return resolve({ api, server });
     });
   });
-}
-exports.stopAPIServer = ({ api }) => {
+};
 
-}
+exports.stopAPIServer = async ({ server, handleTerminationError } = {}) => {
+  if (!server) {
+    return;
+  }
+  await new Promise((resolve) => {
+    server.close((serverCloseError) => {
+      if (serverCloseError) {
+        handleTerminationError({ error: serverCloseError, code: APP_ERROR_CODE.API_TERMINATE });
+      }
+      return resolve();
+    });
+  });
+};
