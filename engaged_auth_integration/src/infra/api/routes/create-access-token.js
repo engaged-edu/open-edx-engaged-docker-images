64d07cd247efcb07491eed7e3b190ae5a0de061b
gustavo.santos97 <gustavoms97@hotmail.com>
2020-08-28 19:16:01 -0300

    feat: add error information and fix spacing

--- engaged_auth_integration/src/infra/api/routes/create-access-token.js
+++ engaged_auth_integration/src/infra/api/routes/create-access-token.js
@@ -5,23 +5,26 @@ const { APP_ERROR_KIND } = require('../../../constants');
 exports.createAccessTokenAPIRouteFactory = ({ AppError, createAccessTokenFromUserEmail } = {}) => {
   return {
     createAccessTokenAPIRoute: async (req, res) => {
-      const { email } = ensureParams({
-        schema: {
-          email: joi.string().email().required()
+      const { email } = ensureParams(
+        {
+          schema: {
+            email: joi.string().email().required(),
+          },
+          params: {
+            email: req.body.email,
+          },
         },
-        params: {
-          email: req.body.email
-        }
-      }, (error) => {
-        throw new AppError({ error, kind: APP_ERROR_KIND.VALIDATION });
-      });
-      
+        (error) => {
+          throw new AppError({ error, kind: APP_ERROR_KIND.VALIDATION });
+        },
+      );
+
       const { user, accessToken } = await createAccessTokenFromUserEmail({ email });
-      
+
       return res.success({
         user,
-        access_token: accessToken
+        access_token: accessToken,
       });
-    }
-  }
-}
+    },
+  };
+};
