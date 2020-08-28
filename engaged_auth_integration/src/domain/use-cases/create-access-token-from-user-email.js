a168d25c1f34715de08567b99a5662eccfc4f421
gustavo.santos97 <gustavoms97@hotmail.com>
2020-08-28 19:17:22 -0300

    feat: add error throwing with more specific information

--- engaged_auth_integration/src/domain/use-cases/create-access-token-from-user-email.js
+++ engaged_auth_integration/src/domain/use-cases/create-access-token-from-user-email.js
@@ -1,14 +1,26 @@
+const { APP_ERROR_CODE } = require('../../../../engaged-auth-integration/src/constants');
+const { APP_ERROR_KIND } = require('../../constants');
 
-
-exports.createAccessTokenFromUserEmailFactory = ({ AppError, createUserAccessToken, fetchUserFromOpenEdx }) => {
+exports.createAccessTokenFromUserEmailFactory = ({ AppError, createOpenEdxUserAccessToken, fetchUserFromOpenEdx }) => {
   return {
     createAccessTokenFromUserEmail: async ({ email } = {}) => {
-      const { user } = await fetchUserFromOpenEdx({ email });
-      if(!user) {
-        //throw error
+      try {
+        const { user } = await fetchUserFromOpenEdx({ email });
+        if (!user) {
+          throw new AppError({
+            code: APP_ERROR_CODE.EDX_USER_NOT_FOUND,
+            error: new Error('User was not found'),
+            kind: APP_ERROR_KIND.NOT_FOUND,
+          });
+        }
+        const { accessToken } = await createOpenEdxUserAccessToken({ userId: user.id });
+        return { user, accessToken };
+      } catch (createAccessTokenFromUserEmailError) {
+        throw new AppError({
+          code: APP_ERROR_CODE.CREATE_TOKEN_FROM_USER_EMAIL,
+          error: createAccessTokenFromUserEmailError,
+        });
       }
-      const { accessToken } = await createUserAccessToken({ userId: user.id });
-      return { user, accessToken };
-    }
-  }
-}
+    },
+  };
+};
