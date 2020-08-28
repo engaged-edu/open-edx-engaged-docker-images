04a2c7e5f47ef15bcc054d0f51940add27389bf3
gustavo.santos97 <gustavoms97@hotmail.com>
2020-08-28 19:14:13 -0300

    feat: add information data to the error thrown to the express middleware

--- engaged_auth_integration/src/infra/api/middleware/request-authentication.js
+++ engaged_auth_integration/src/infra/api/middleware/request-authentication.js
@@ -1,11 +1,18 @@
+const { APP_ERROR_CODE, APP_ERROR_KIND } = require('../../../constants');
 
 exports.requestAuthenticationAPIMiddlewareFactory = ({ ENV, AppError }) => {
   return {
     requestAuthenticationAPIMiddleware: (req, res, next) => {
-      if(req.headers['Authorization'] !== ENV.API_SECRET_KEY) {
-        return res.catch(new AppError(new Error()));
+      if (req.headers['Authorization'] !== ENV.API_SECRET_KEY) {
+        return res.catch(
+          new AppError({
+            error: new Error('Missing secret key'),
+            code: APP_ERROR_CODE.NO_SECRET_KEY,
+            kind: APP_ERROR_KIND.FORBIDDEN,
+          }),
+        );
       }
       return next();
-    }
-  }
-}
+    },
+  };
+};
