ce442f164eb5610b8029416eb331e96a1bb639bc
gustavo.santos97 <gustavoms97@hotmail.com>
2020-09-04 14:56:24 -0300

    fix: fix aws env var name

--- mysql-source-connector/src/infra/sdk/aws.js
+++ mysql-source-connector/src/infra/sdk/aws.js
@@ -5,7 +5,7 @@ const AWS = require('aws-sdk');
  */
 exports.configAWSSDK = ({ ENV } = {}) => {
   AWS.config.update({
-    accessKeyId: ENV.ENGAGED_AWS_ACCESS_KEY,
+    accessKeyId: ENV.AWS_ACCESS_KEY,
     secretAccessKey: ENV.AWS_SECRET_KEY,
     region: ENV.AWS_REGION,
   });
